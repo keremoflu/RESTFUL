ca333a21451911b3c1918142d979327379cf78f2
Kerem Oflu <keremoflu92@gmail.com>
2018-04-07 20:45:02 +0300

    Add files via upload

---
+++ PHP Basics/PDO_Basics.php
@@ -0,0 +1,39 @@
+<?php
+
+try{
+    $connect = new PDO('mysql:host=127.0.0.1; dbname=pdodata', 'root', '' );
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e){
+    //echo $e->getMessage();
+    die();
+}
+
+//fetch all
+$query = $connect->query('SELECT * FROM pdovalues');
+while($row = $query->fetch()){
+    echo $row['user_name']."<br>";
+}
+
+//avoid sql injection (string escape)
+$myValue = "John";
+$sql = "INSERT INTO pdovalues (user_name) VALUES (:user_name)";
+$sql_query = $connect->prepare($sql);
+$sql_query->execute(array(
+    ':user_name' => $myValue,
+));
+
+
+//pdo fetch - only user_name column values
+$query_fetch = $connect->query("SELECT * FROM pdovalues");
+while($row = $query_fetch->fetch(PDO::FETCH_OBJ)){
+    echo "------------";
+    echo $row->user_name."<br>";
+}
+
+//get row count
+$query_count = $connect->query("SELECT * FROM pdovalues");
+if($query_count->rowCount()){
+    echo $query_count->rowCount();
+}else{
+    echo "no result";
+}
